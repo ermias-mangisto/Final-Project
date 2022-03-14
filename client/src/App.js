e06986ce036c0153cd5a8e24aa57c262741c353b
YafitSamuel <gyafit642@gmail.com>
2022-03-14 16:43:09 +0200

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 
 import "./App.css";
-import Footer from "./components/parts/footer/footer";
-import NavBar from "./components/parts/navbar/navbar";
 import Landing from "./components/pages/landing/landing";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-        <NavBar />
+     
         <Landing />
-        <Footer />
-      {/* </header> */}
+    
     </div>
   );
 }
