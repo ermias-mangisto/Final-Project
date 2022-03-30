599bd9b4567470d4748b1ce7e716263ae8664dfb
ermias-mangisto <ermiasmangisto@gmail.com>
2022-03-30 21:08:57 +0300

    .

--- client/src/components/parts/alerts/checkAlert.jsx
+++ client/src/components/parts/alerts/checkAlert.jsx
@@ -15,7 +15,7 @@ const CheckAlert = () => {
                 alerts?
                 alerts.map(data => {
                     if (localStorage.getItem("newAlert") === null) {
-                        localStorage.setItem("newAlert", `${alert[alert.length - 1].createdAt}`);
+                        localStorage.setItem("newAlert", `${alerts[alerts.length - 1].createdAt}`);
                     }else if (data.receiverUserId == user._id) {
                         if (new Date(localStorage.getItem("newAlert")) < new Date(data.createdAt)) {
                             count++
